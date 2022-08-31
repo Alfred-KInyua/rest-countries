79e519247133a28bab42746eb0efe9132db8d29a
Kinyua <alfkinyua@gmail.com>
2022-08-31 16:42:04 +0300

    Add header files

---
+++ src/components/Access/Header.js
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { FaGlobe } from 'react-icons/fa';
+
+const Header = ({ title, number }) => (
+  <div className="headers">
+    <NavLink to={`/continent/${title}`} className="navLink">
+      <FaGlobe />
+      <div className="resource">
+        <h3>{title}</h3>
+        <p>
+          <strong>{number}</strong>
+          <strong>Countries</strong>
+        </p>
+      </div>
+    </NavLink>
+  </div>
+);
+
+Header.defaultProps = {
+  title: null,
+  number: null,
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+  number: PropTypes.number,
+};
+
+export default Header;
